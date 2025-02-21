591462704b27bdbac41b74edea28451cfd34235e
jayant <jayant746@gmail.com>
2025-02-21 14:38:13 -0500

    add open ai key mapping in get_chat_model

--- src/paperweave/model.py
+++ src/paperweave/model.py
@@ -4,7 +4,9 @@ from langchain_openai import ChatOpenAI
 
 
 def get_chat_model() -> ChatOllama | ChatOpenAI:
-    if "OPENAI_API_KEY" in os.environ and os.environ["OPENAI_API_KEY"]:
+    if "OPENAI_KEY" in os.environ and os.environ["OPENAI_KEY"]:
+        os.environ["OPENAI_API_KEY"] = os.environ["OPENAI_KEY"]
         return ChatOpenAI(model="gpt-4o-mini")
     else:
-        return ChatOllama(model="mistral-small:latest")
+        print("no OPENAI_KEY set, using llama3.1:latest")
+        return ChatOllama(model="llama3.1:latest")
